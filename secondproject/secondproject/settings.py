9e7e9dbefd20d31eaa5e005bf91cc5fc06e4a58c
sue991 <sue991@naver.com>
2021-01-08 01:37:37 +0900

    heroku

--- secondproject/secondproject/settings.py
+++ secondproject/secondproject/settings.py
@@ -134,6 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-import dj_database_url 
-db_from_env = dj_database_url.config(conn_max_age=500) 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
